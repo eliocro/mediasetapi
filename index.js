96839e5264fb09e3409cfe9f02391ec9a38da8f3
Élio Cró <elio@front.no>
2015-12-15 18:17:58 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Hapi = require('hapi');
+const mediaset = require('./media-set');
+
+
+var server = new Hapi.Server();
+server.connection({
+  port: process.env.PORT || 5000,
+  routes: {
+    cors: true
+  }
+});
+
+server.route([
+  {
+    method: '*',
+    path:'/',
+    handler: (request, reply) => {
+      reply('MediaSet API');
+    }
+  },
+  {
+    method: 'GET',
+    path:'/mediaset',
+    handler: mediaset.list
+  },
+]);
+
+server.start(() => {
+  console.log('MediaSet API:', 'Started on ' + server.info.port);
+});
---
+++ media-set.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const li = require('lorem-ipsum');
+
+const count = 50;
+const images  = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife',
+ 'fashion', 'people', 'nature', 'sports', 'technics', 'transport'];
+
+
+var entries = [];
+for(let i = 0; i < count; i++) {
+
+  let item = {
+    title: 'Set ' + Math.floor(Math.random()*1000000)
+  };
+  entries.push(item);
+
+  item.media = [];
+  let num = Math.ceil(Math.random()*6);
+
+  for(let j = 0; j < num; j++) {
+    let img = Math.floor(Math.random()*13);
+    let gray = !!Math.floor(Math.random()*2);
+    let words = Math.floor(Math.random()*5) + 5;
+
+    item.media.push({
+      text: li({
+        count: words,
+        units: 'words'
+      }),
+      image: `http://lorempixel.com/${ gray && 'g/' || '' }133/100/${ images[img] }`,
+      link: 'http://www.google.com'
+    });
+  }
+}
+
+
+module.exports = {
+  list (request, reply) {
+    let qs = request.query;
+    let res = [];
+
+    let start = parseInt(qs.start) || 0;
+    let rows = parseInt(qs.rows) || 10;
+
+    for(let i = 0; i < rows && (start+i) < count; i++) {
+      res.push(entries[start+i]);
+    }
+    reply({
+      found: count,
+      docs: res
+    });
+  }
+};
